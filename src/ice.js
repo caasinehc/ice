7cd221e040a204d648fd7521d31643aa303a113f
caasinehc <31968674+caasinehc@users.noreply.github.com>
2017-11-19 17:01:44 -0500

    ice.js loads all working modules
    + ice.js now loads all working ice modules

--- src/ice.js
+++ src/ice.js
@@ -1,7 +1,29 @@
 var ice = (function (ice) {
 
-	ice.version = "v2.0.0"
+	ice.version = "v2.1.0"
 	ice.modules = [];
 
 	return ice;
 }(ice || {}));
+
+// Import all ice modules
+
+(function() {
+
+	var commitHash = "c46a0273e1f0a58cc937f8d3c44c1a616437a961"
+
+	var urlStart = "https://cdn.rawgit.com/caasinehc/ice/"
+
+	function importFile(file) {
+		var script = document.createElement("script");
+		script.src = urlStart + commitHash + file;
+		document.head.appendChild(script);
+	}
+
+	importFile("src/ice.math.js");
+	importFile("src/ice.colors.js");
+	// importFile("src/ice.graphics.js");
+	importFile("src/ice.debug.js");
+	// importFile("src/ice.physics.js");
+
+})();
