e01cb74c6724af431965e09226117d2a4a65d413
caasinehc <31968674+caasinehc@users.noreply.github.com>
2017-11-19 15:20:41 -0500

    v2.0.0 Major rewrite
    > Completely rewrote the way the library is created to be more reliable/consistent

--- src/ice.debug.js
+++ src/ice.debug.js
@@ -1,45 +1,54 @@
-/*
- *	debug module
- */
- 
-ice.debug.version = "v1.0.1"; // This version of the ice.debug module
+var ice = (function (ice) {
 
-// Functions
-ice.debug.testFunction = function(testFunction, sampleSize, args) {
-	// Stores results
-	var results = {}
-	// Stores info on results
-	var min;
-	var max;
-	var resultsText = {}
-	var totalResults = 0;
-	// Runs the function [sampleSize] times, incrementing results[(the result)]
-	for(var i = 0; i < sampleSize; i++) {
-		var result = testFunction.apply(null, args);
-		if(typeof results[result] === "undefined") {
-			results[result] = 1;
+	ice.modules = ice.modules || [];
+	ice.modules.push("debug");
+	ice.debug = {};
+	ice.debug.version = "v2.0.0"; // This version of the ice.debug module
+
+	/*
+	 *	================ Debug Module ================
+	 */
+
+	// Methods
+
+	ice.debug.testFunction = function(func, sampleSize, args) {
+		// Stores results
+		var results = {};
+		// Stores info on results
+		var min;
+		var max;
+		var resultsText = {};
+		var totalResults = 0;
+		// Runs the function [sampleSize] times, incrementing results[(the result)]
+		for(var i = 0; i < sampleSize; i++) {
+			var result = func.apply(null, args);
+			if(typeof results[result] === "undefined") {
+				results[result] = 1;
+			}
+			else {
+				results[result]++;
+			}
 		}
-		else {
-			results[result]++;
+		// loops through results, noting important values and adding percentage information
+		for(var key in results) {
+			if(typeof min === "undefined") {min = key; max = key}
+			if(results[key] < results[min]) {min = key;}
+			if(results[key] > results[max]) {max = key;}
+			totalResults++;
+			resultsText[key] = results[key] + " (" + ((results[key] / sampleSize) * 100).toFixed(2) + "%)";
 		}
+		// The information to be returned
+		var returnObject = {
+			"Function tested": func.toString(),
+			"Sample size": sampleSize,
+			"Total unique results": totalResults,
+			"Expected mean (assuming even distribution)": sampleSize / totalResults + "(" + (100 / totalResults).toFixed(2) + "%)",
+			"Most common": max + ", occuring a whopping " + results[max] + " times (" + ((results[max] / sampleSize) * 100).toFixed(2) + "%)",
+			"Least common": min + ", occuring only " + results[min] + " times (" + ((results[min] / sampleSize) * 100).toFixed(2) + "%)",
+			"Results": resultsText
+		}
+		return returnObject;
 	}
-	// loops through results, noting important values and adding percentage information
-	for(var key in results) {
-		if(typeof min === "undefined") {min = key; max = key}
-		if(results[key] < results[min]) {min = key;}
-		if(results[key] > results[max]) {max = key;}
-		totalResults++;
-		resultsText[key] = results[key] + " (" + ((results[key] / sampleSize) * 100).toFixed(2) + "%)";
-	}
-	// The information to be returned
-	var returnObject = {
-		"Function tested": testFunction.toString(),
-		"Sample size": sampleSize,
-		"Total unique results": totalResults,
-		"Expected mean (assuming even distribution)": sampleSize / totalResults + "(" + (100 / totalResults).toFixed(2) + "%)",
-		"Most common": max + ", occuring a whopping " + results[max] + " times (" + ((results[max] / sampleSize) * 100).toFixed(2) + "%)",
-		"Least common": min + ", occuring only " + results[min] + " times (" + ((results[min] / sampleSize) * 100).toFixed(2) + "%)",
-		"Results": resultsText
-	}
-	return returnObject;
-}
+
+	return ice;
+}(ice || {}));
--- src/ice.js
+++ src/ice.js
@@ -1,14 +1,7 @@
-/*
- *	Declaring the ice object and various modules
- */
-var ice = {} // Main ice object
-ice.math = {} // Everything mathmatical or number related
-ice.graphics = {} // Everything graphics or GUI related
-ice.physics = {} // Everything physics related
-ice.colors = {} // Everything color related
-ice.debug = {} // Everything debugging related
+var ice = (function (ice) {
 
-/*
- *	Meta information
- */
-ice.version = "v1.0.0"; // This version of the ice library
+	ice.version = "v2.0.0"
+	ice.modules = [];
+
+	return ice;
+}(ice || {}));
