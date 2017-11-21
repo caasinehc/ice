8dc5aa43f1a399df6834df9ec687e16934caa07c
Isaac Chen <31968674+caasinehc@users.noreply.github.com>
2017-11-21 12:26:46 -0500

    [insert generic, not-helpful commit message here]
    Please see title :)

--- src/ice.physics.js
+++ src/ice.physics.js
@@ -11,6 +11,16 @@ var ice = (function (ice) {
 
 	// Constructors
 
+	/*
+	 *	rotate (deg and rad) (by and to)
+	 *	set and get angle (deg and rad)
+	 *	randomize (x, y, xy, xor, angle, and magnitude)
+	 *	dot and cross
+	 *	distance (x, y, euclidian, manhattan) (absolute and not absolute) (squared and not)
+	 *	angle stuff (angleTo, slerp)
+	 *	limit (set and multiply)
+	 *	get min and max xy values
+	 */
 	ice.physics.Vector = function(x, y) {
 		if(!(this instanceof ice.physics.Vector)) {
 			return new ice.physics.Vector(x, y);
@@ -34,7 +44,7 @@ var ice = (function (ice) {
 	ice.physics.Vector.prototype.mag = ice.physics.Vector.prototype.magnitude;
 	ice.physics.Vector.prototype.length = ice.physics.Vector.prototype.magnitude;
 
-	// Faster than .magnitude() (for comparison)
+	// Faster than .magnitude() (For comparison)
 	ice.physics.Vector.prototype.magnitudeSq = function() {
 		return this.x * this.x + this.y * this.y;
 	}
@@ -45,42 +55,64 @@ var ice = (function (ice) {
 	ice.physics.Vector.prototype.equals = function(vec) {
 		return this.x === vec.x && this.y === vec.y;
 	}
+	
+	// Returns the vector as a string (Mostly for debugging)
+	ice.physics.Vector.prototype.toString = function() {
+		return "[object ice.physics.Vector] {x: " + this.x + ", y: " + this.y + "}";
+	}
+	
+	// Returns the vector as an object (Mostly for debugging)
+	ice.physics.Vector.prototype.toObject = function() {
+		return {x: this.x, y: this.y};
+	}
+	
+	// Returns the vector as an array (Mostly for debugging)
+	ice.physics.Vector.prototype.toArray = function() {
+		return [this.x, this.y];
+	}
 
 	// Normalizes the vector (The magnitude is set to 1, but the angle is unchanged)
 	ice.physics.Vector.prototype.normalize = function() {
-		return this.div(this.magnitude() || 1);
+		return this.divide(this.magnitude() || 1);
 	}
 	ice.physics.Vector.prototype.norm = ice.physics.Vector.prototype.normalize;
 
 	// Sets the magnitude (The angle remains unchanged)
 	ice.physics.Vector.prototype.setMagnitude = function(mag) {
-		return this.normalize().mult(mag);
+		return this.normalize().multiply(mag);
 	}
 	ice.physics.Vector.prototype.scale = ice.physics.Vector.prototype.setMagnitude;
 	ice.physics.Vector.prototype.setMag = ice.physics.Vector.prototype.setMagnitude;
 
 	// Moves the vector towards another (By absolute distance)
 	ice.physics.Vector.prototype.towards = function(vec, dist) {
-		return this.add(vec.clone().sub(this).normalize().mult(dist));
+		return this.add(vec.clone().subtract(this).normalize().multiply(dist));
 	}
 
 	// Moves the vector towards another (By relative distance)
 	ice.physics.Vector.prototype.linearInterpolate = function(vec, frac) {
-		return this.add(vec.clone().sub(this).mult(frac));
+		return this.add(vec.clone().subtract(this).multiply(frac));
 	}
 	ice.physics.Vector.prototype.linInt = ice.physics.Vector.prototype.linearInterpolate;
 	ice.physics.Vector.prototype.lerp = ice.physics.Vector.prototype.linearInterpolate;
 
 	// Inverts the vector (x and/or y *= -1)
 	ice.physics.Vector.prototype.invert = function() {
-		return this.mult(-1);
+		return this.multiply(-1);
 	}
 	ice.physics.Vector.prototype.invertX = function() {
-		return this.multX(-1);
+		return this.multiplyX(-1);
 	}
 	ice.physics.Vector.prototype.invertY = function() {
-		return this.multY(-1);
+		return this.multiplyY(-1);
 	}
+	
+	ice.physics.Vector.prototype.round = function() {
+		this.x = Math.round(this.x);
+		this.y = Math.round(this.y);
+		return this;
+	}
+	ice.physics.Vector.prototype.unfloat = ice.physics.Vector.prototype.round;
 
 	// Sets the vectors x and y
 	ice.physics.Vector.prototype.set = function(vec) {
