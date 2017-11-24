99560c7ae6c63b8cd34186e002bd2d9ed2187956
Isaac Chen <31968674+caasinehc@users.noreply.github.com>
2017-11-23 19:17:41 -0500

    Now logs progress and times the test

--- src/ice.debug.js
+++ src/ice.debug.js
@@ -19,6 +19,8 @@ var ice = (function (ice) {
 		var max;
 		var resultsText = {};
 		var totalResults = 0;
+		var percent = Math.floor(sampleSize / 100);
+		var before = performance.now();
 		// Runs the function [sampleSize] times, incrementing results[(the result)]
 		for(var i = 0; i < sampleSize; i++) {
 			var result = func.apply(null, args);
@@ -28,7 +30,11 @@ var ice = (function (ice) {
 			else {
 				results[result]++;
 			}
+			if(i % percent === 0) {
+				console.log("Test progress: " + Math.floor(i / percent) + "%");
+			}
 		}
+		var after = performance.now();
 		// loops through results, noting important values and adding percentage information
 		for(var key in results) {
 			if(typeof min === "undefined") {min = key; max = key}
@@ -37,10 +43,19 @@ var ice = (function (ice) {
 			totalResults++;
 			resultsText[key] = results[key] + " (" + ((results[key] / sampleSize) * 100).toFixed(2) + "%)";
 		}
+		var totalSeconds = (after - before) / 1000;
+		var totalTime = "";
+		if(totalSeconds >= 60) {
+			totalTime += Math.floor(totalSeconds / 60) + " Minutes, ";
+		}
+		totalTime += totalSeconds % 60 + " Seconds";
 		// The information to be returned
 		var returnObject = {
 			"Function tested": func.toString(),
 			"Sample size": sampleSize,
+			"Total unique results": totalResults,
+			"Total time": totalTime,
+			"Average time": ((after - before) / sampleSize) + " milliseconds",
 			"Total unique results": totalResults,
 			"Expected mean (assuming even distribution)": sampleSize / totalResults + "(" + (100 / totalResults).toFixed(2) + "%)",
 			"Most common": max + ", occuring a whopping " + results[max] + " times (" + ((results[max] / sampleSize) * 100).toFixed(2) + "%)",
