d53ffdbd86825511919fa3abe78b41bbe2db2db2
Isaac Chen <31968674+caasinehc@users.noreply.github.com>
2017-11-30 17:18:15 -0500

    Now imports graphics!

--- src/ice.js
+++ src/ice.js
@@ -1,6 +1,6 @@
 var ice = (function (ice) {
 
-	ice.version = "v2.1.3"
+	ice.version = "v2.1.4"
 	ice.modules = [];
 	console.log("ice " + ice.version + " imported successfully.");
 	console.log("Importing all modules...");
@@ -19,7 +19,7 @@ var ice = (function (ice) {
 
 	importFile("src/ice.math.js");
 	importFile("src/ice.colors.js");
-	// importFile("src/ice.graphics.js");
+	importFile("src/ice.graphics.js");
 	importFile("src/ice.debug.js");
 	importFile("src/ice.physics.js");
 
