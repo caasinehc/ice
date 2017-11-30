126fd59e2f929c0700a9ef4be8843e59d394a4ae
caasinehc <31968674+caasinehc@users.noreply.github.com>
2017-11-30 17:16:40 -0500

    Added tons
    ice.graphics is pretty much ready to go!

--- src/ice.graphics.js
+++ src/ice.graphics.js
@@ -3,7 +3,7 @@ var ice = (function (ice) {
 	ice.modules = ice.modules || [];
 	ice.modules.push("graphics");
 	ice.graphics = {};
-	ice.graphics.version = "v2.0.0"; // This version of the ice.graphics module
+	ice.graphics.version = "v2.1.0"; // This version of the ice.graphics module
 	console.log("ice.graphics " + ice.graphics.version + " imported successfully.");
 
 	/*
@@ -28,7 +28,12 @@ var ice = (function (ice) {
 		}
 	}
 	var WHITE = "#FFFFFF";
-	var SEPIA = "#704214";
+	function applyCssFilter(filter, value) {
+		var oldFilter = ctx.filter;
+		ctx.filter = filter + "(" + value + ")"
+		ctx.drawImage(canvas, 0, 0);
+		ctx.filter = oldFilter;
+	}
 
 	// Constructors
 
@@ -48,10 +53,13 @@ var ice = (function (ice) {
 	}
 	ice.graphics.Scene.prototype.clear = function() {
 		var oldGCO = ctx.globalCompositeOperation;
+		var oldFilter = ctx.filter;
 		ctx.globalCompositeOperation = "source-over";
+		ctx.filter = "none";
 		ctx.fillStyle = bgColor;
 		ctx.fillRect(0, 0, width, height);
 		ctx.globalCompositeOperation = oldGCO;
+		ctx.filter = oldFilter;
 	}
 	ice.graphics.Scene.prototype.download = function(name) {
 		var link = document.createElement("a");
@@ -59,36 +67,47 @@ var ice = (function (ice) {
 		link.download = name || "download";
 		link.click();
 	}
-	ice.graphics.Scene.prototype.invert = function() {
-		var oldGCO = ctx.globalCompositeOperation;
-		var oldFillStyle = ctx.fillStyle;
-		ctx.globalCompositeOperation = "difference";
-		ctx.fillStyle = WHITE;
-		ctx.fillRect(0, 0, width, height);
-		ctx.globalCompositeOperation = oldGCO;
-		ctx.fillStlye = oldFillStyle;
+	ice.graphics.Scene.prototype.invert = function(percent) {
+		percent = percent === undefined ? "100%"  : percent + "%";
+		applyCssFilter("invert", percent);
+	}
+	ice.graphics.Scene.prototype.grayscale = function(percent) {
+		percent = percent === undefined ? "100%"  : percent + "%";
+		applyCssFilter("grayscale", percent);
+	}
+	ice.graphics.Scene.prototype.sepia = function(percent) {
+		percent = percent === undefined ? "100%"  : percent + "%";
+		applyCssFilter("sepia", percent);
+	}
+	ice.graphics.Scene.prototype.brightness = function(percent) {
+		percent = percent === undefined ? "100%"  : percent + "%";
+		applyCssFilter("brightness", percent);
+	}
+	ice.graphics.Scene.prototype.contrast = function(percent) {
+		percent = percent === undefined ? "100%"  : percent + "%";
+		applyCssFilter("contrast", percent);
+	}
+	ice.graphics.Scene.prototype.blur = function(px) {
+		px = px === undefined ? "1px" : px + "px";
+		applyCssFilter("blur", px);
+	}
+	ice.graphics.Scene.prototype.saturate = function(percent) {
+		percent = percent === undefined ? "100%"  : percent + "%";
+		applyCssFilter("saturate", percent);
+	}
+	ice.graphics.Scene.prototype.opacity = function(percent) {
+		percent = percent === undefined ? "100%"  : percent + "%";
+		applyCssFilter("opacity", percent);
+	}
+	ice.graphics.Scene.prototype.colorshift = function(degrees) {
+		degrees = degrees === undefined ? "180deg"  : degrees + "deg";
+		applyCssFilter("hue-rotate", degrees);
 	}
 	ice.graphics.Scene.prototype.recolor = function(color) {
 		var oldGCO = ctx.globalCompositeOperation;
 		var oldFillStyle = ctx.fillStyle;
 		ctx.globalCompositeOperation = "color";
 		ctx.fillStyle = color || WHITE;
-		ctx.fillRect(0, 0, width, height);
-		ctx.globalCompositeOperation = oldGCO;
-		ctx.fillStyle = oldFillStyle;
-	}
-	ice.graphics.Scene.prototype.grayscale = function() {
-		this.recolor(WHITE);
-	}
-	ice.graphics.Scene.prototype.sepia = function() {
-		this.recolor(SEPIA);
-	}
-	ice.graphics.Scene.prototype.saturate = function(percent) {
-		percent = percent === undefined ? 100 : percent;
-		var oldGCO = ctx.globalCompositeOperation;
-		var oldFillStyle = ctx.fillStyle;
-		ctx.globalCompositeOperation = "saturation";
-		ctx.fillStyle = "hsl(0, " + percent + "%, 50%)";
 		ctx.fillRect(0, 0, width, height);
 		ctx.globalCompositeOperation = oldGCO;
 		ctx.fillStyle = oldFillStyle;
