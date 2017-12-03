eed1a4e0ef6fe22b6c2296d1e6ffd61df337c6de
caasinehc <31968674+caasinehc@users.noreply.github.com>
2017-12-03 14:54:26 -0500

    Again, added stuff

--- src/ice.graphics.js
+++ src/ice.graphics.js
@@ -3,7 +3,7 @@ var ice = (function (ice) {
 	ice.modules = ice.modules || [];
 	ice.modules.push("graphics");
 	ice.graphics = {};
-	ice.graphics.version = "v2.1.1"; // This version of the ice.graphics module
+	ice.graphics.version = "v2.1.2"; // This version of the ice.graphics module
 	console.log("ice.graphics " + ice.graphics.version + " imported successfully.");
 
 	/*
@@ -35,10 +35,10 @@ var ice = (function (ice) {
 	}
 	var WHITE = "#FFFFFF";
 	var BLACK = "#000000";
-	function applyCssFilter(filter, value) {
+	function applyCssFilter(ctx, filter, value) {
 		var oldFilter = ctx.filter;
 		ctx.filter = filter + "(" + value + ")"
-		ctx.drawImage(canvas, 0, 0);
+		ctx.drawImage(ctx.canvas, 0, 0);
 		ctx.filter = oldFilter;
 	}
 
@@ -49,25 +49,24 @@ var ice = (function (ice) {
 			return new ice.graphics.Scene(ctxInput, bgColorInput);
 		}
 
-		ctx = interpretCtx(ctxInput);
-		canvas = ctx.canvas;
-		bgColor = bgColorInput || WHITE;
-		width = canvas.width;
-		height = canvas.height;
-	}
-	ice.graphics.Scene.prototype.setBgColor = function(newBgColor) {
-		bgColor = newBgColor || bgColor;
+		this.ctx = interpretCtx(ctxInput);
+		this.canvas = this.ctx.canvas;
+		this.bgColor = bgColorInput || WHITE;
+		this.width = this.canvas.width;
+		this.height = this.canvas.height;
+		this.midWidth = this.width / 2;
+		this.midHeight = this.height / 2;
 	}
 	ice.graphics.Scene.prototype.clear = function() {
-		var oldGCO = ctx.globalCompositeOperation;
-		var oldFilter = ctx.filter;
-		ctx.globalCompositeOperation = "source-over";
-		ctx.filter = "none";
-		ctx.fillStyle = bgColor;
-		ctx.clearRect(0, 0, width, height);
-		ctx.fillRect(0, 0, width, height);
-		ctx.globalCompositeOperation = oldGCO;
-		ctx.filter = oldFilter;
+		var oldGCO = this.ctx.globalCompositeOperation;
+		var oldFilter = this.ctx.filter;
+		this.ctx.globalCompositeOperation = "source-over";
+		this.ctx.filter = "none";
+		this.ctx.fillStyle = this.bgColor;
+		this.ctx.clearRect(0, 0, this.width, this.height);
+		this.ctx.fillRect(0, 0, this.width, this.height);
+		this.ctx.globalCompositeOperation = oldGCO;
+		this.ctx.filter = oldFilter;
 	}
 	ice.graphics.Scene.prototype.download = function(name) {
 		/*
@@ -77,129 +76,134 @@ var ice = (function (ice) {
 		 *	For more info, visit: https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
 		 */
 		var link = document.createElement("a");
-		link.href = canvas.toDataURL();
+		link.href = this.canvas.toDataURL();
 		link.download = name || "download";
 		link.click();
 	}
+	ice.graphics.Scene.prototype.move = function(x, y) {
+		var content = this.ctx.getImageData(0, 0, this.width, this.height);
+		this.clear();
+		this.ctx.putImageData(content, x, y);
+	}
 	ice.graphics.Scene.prototype.invert = function(percent) {
 		percent = percent === undefined ? "100%"  : percent + "%";
-		applyCssFilter("invert", percent);
+		applyCssFilter(this.ctx, "invert", percent);
 	}
 	ice.graphics.Scene.prototype.grayscale = function(percent) {
 		percent = percent === undefined ? "100%"  : percent + "%";
-		applyCssFilter("grayscale", percent);
+		applyCssFilter(this.ctx, "grayscale", percent);
 	}
 	ice.graphics.Scene.prototype.sepia = function(percent) {
 		percent = percent === undefined ? "100%"  : percent + "%";
-		applyCssFilter("sepia", percent);
+		applyCssFilter(this.ctx, "sepia", percent);
 	}
 	ice.graphics.Scene.prototype.brightness = function(percent) {
 		percent = percent === undefined ? "100%"  : percent + "%";
-		applyCssFilter("brightness", percent);
+		applyCssFilter(this.ctx, "brightness", percent);
 	}
 	ice.graphics.Scene.prototype.contrast = function(percent) {
 		percent = percent === undefined ? "100%"  : percent + "%";
-		applyCssFilter("contrast", percent);
+		applyCssFilter(this.ctx, "contrast", percent);
 	}
 	ice.graphics.Scene.prototype.blur = function(px) {
 		px = px === undefined ? "1px" : px + "px";
-		applyCssFilter("blur", px);
+		applyCssFilter(this.ctx, "blur", px);
 	}
 	ice.graphics.Scene.prototype.saturate = function(percent) {
 		percent = percent === undefined ? "100%"  : percent + "%";
-		applyCssFilter("saturate", percent);
+		applyCssFilter(this.ctx, "saturate", percent);
 	}
 	ice.graphics.Scene.prototype.opacity = function(percent) {
 		percent = percent === undefined ? "100%"  : percent + "%";
-		applyCssFilter("opacity", percent);
+		applyCssFilter(this.ctx, "opacity", percent);
 	}
 	ice.graphics.Scene.prototype.colorshift = function(degrees) {
 		degrees = degrees === undefined ? "180deg"  : degrees + "deg";
-		applyCssFilter("hue-rotate", degrees);
+		applyCssFilter(this.ctx, "hue-rotate", degrees);
 	}
 	ice.graphics.Scene.prototype.recolor = function(color) {
-		var oldGCO = ctx.globalCompositeOperation;
-		var oldFillStyle = ctx.fillStyle;
-		ctx.globalCompositeOperation = "color";
-		ctx.fillStyle = color || WHITE;
-		ctx.fillRect(0, 0, width, height);
-		ctx.globalCompositeOperation = oldGCO;
-		ctx.fillStyle = oldFillStyle;
+		var oldGCO = this.ctx.globalCompositeOperation;
+		var oldFillStyle = this.ctx.fillStyle;
+		this.ctx.globalCompositeOperation = "color";
+		this.ctx.fillStyle = color || WHITE;
+		this.ctx.fillRect(0, 0, this.width, this.height);
+		this.ctx.globalCompositeOperation = oldGCO;
+		this.ctx.fillStyle = oldFillStyle;
 	}
 	ice.graphics.Scene.prototype.circle = function(x, y, rad, color) {
-		ctx.beginPath();
-		ctx.arc(x, y, rad, 0, TAU);
-		ctx.fillStyle = color || BLACK;
-		ctx.fill();
+		this.ctx.beginPath();
+		this.ctx.arc(x, y, rad, 0, TAU);
+		this.ctx.fillStyle = color || BLACK;
+		this.ctx.fill();
 	}
 	ice.graphics.Scene.prototype.circleOutline = function(x, y, rad, thickness, color) {
-		ctx.beginPath();
-		ctx.arc(x, y, rad, 0, TAU);
-		ctx.lineWidth = thickness || 1;
-		ctx.strokeStyle = color || BLACK;
-		ctx.stroke();
+		this.ctx.beginPath();
+		this.ctx.arc(x, y, rad, 0, TAU);
+		this.ctx.lineWidth = thickness || 1;
+		this.ctx.strokeStyle = color || BLACK;
+		this.ctx.stroke();
 	}
 	ice.graphics.Scene.prototype.square = function(x, y, width, color) {
 		var midWidth = width / 2;
-		ctx.fillStyle = color || BLACK;
-		ctx.fillRect(x - midWidth, y - midWidth, width, width);
+		this.ctx.fillStyle = color || BLACK;
+		this.ctx.fillRect(x - midWidth, y - midWidth, width, width);
 	}
 	ice.graphics.Scene.prototype.squareOutline = function(x, y, width, thickness, color) {
 		var midWidth = width / 2;
-		ctx.lineWidth = thickness || 1;
-		ctx.strokeStyle = color || BLACK;
-		ctx.strokeRect(x - midWidth, y - midWidth, width, width);
+		this.ctx.lineWidth = thickness || 1;
+		this.ctx.strokeStyle = color || BLACK;
+		this.ctx.strokeRect(x - midWidth, y - midWidth, width, width);
 	}
 	ice.graphics.Scene.prototype.triangle = function(x1, y1, x2, y2, x3, y3, color) {
-		ctx.beginPath();
-		ctx.moveTo(x1, y1);
-		ctx.lineTo(x2, y2);
-		ctx.lineTo(x3, y3);
-		ctx.fillStyle = color || BLACK;
-		ctx.fill();
+		this.ctx.beginPath();
+		this.ctx.moveTo(x1, y1);
+		this.ctx.lineTo(x2, y2);
+		this.ctx.lineTo(x3, y3);
+		this.ctx.fillStyle = color || BLACK;
+		this.ctx.fill();
 	}
 	ice.graphics.Scene.prototype.triangleOutline = function(x1, y1, x2, y2, x3, y3, thickness, color) {
-		ctx.beginPath();
-		ctx.moveTo(x1, y1);
-		ctx.lineTo(x2, y2);
-		ctx.lineTo(x3, y3);
-		ctx.closePath();
-		ctx.lineWidth = thickness || 1;
-		ctx.strokeStyle = color || BLACK;
-		ctx.stroke();
+		this.ctx.beginPath();
+		this.ctx.moveTo(x1, y1);
+		this.ctx.lineTo(x2, y2);
+		this.ctx.lineTo(x3, y3);
+		this.ctx.closePath();
+		this.ctx.lineWidth = thickness || 1;
+		this.ctx.strokeStyle = color || BLACK;
+		this.ctx.stroke();
 	}
 	ice.graphics.Scene.prototype.rect = function(x, y, width, height, color) {
-		ctx.fillStyle = color || BLACK;
-		ctx.fillRect(x - width / 2, y - height / 2, width, height);
+		this.ctx.fillStyle = color || BLACK;
+		this.ctx.fillRect(x, y, width, height);
 	}
 	ice.graphics.Scene.prototype.rectOutline = function(x, y, width, height, thickness, color) {
 		ctx.lineWidth = thickness || 1;
 		ctx.strokeStyle = color || BLACK;
-		ctx.strokeRect(x - width / 2, y - height / 2, width, height);
+		ctx.strokeRect(x, y, width, height);
 	}
 	ice.graphics.Scene.prototype.line = function(x1, y1, x2, y2, thickness, color, cap) {
-		ctx.lineWidth = thickness || 1;
-		ctx.strokeStyle = color || BLACK;
-		ctx.lineCap = cap || "butt";
-		ctx.beginPath();
-		ctx.moveTo(x1, y1);
-		ctx.lineTo(x2, y2);
-		ctx.stroke();
+		this.ctx.lineWidth = thickness || 1;
+		this.ctx.strokeStyle = color || BLACK;
+		this.ctx.lineCap = cap || "butt";
+		this.ctx.beginPath();
+		this.ctx.moveTo(x1, y1);
+		this.ctx.lineTo(x2, y2);
+		this.ctx.stroke();
 	}
 	ice.graphics.Scene.prototype.text = function(text, x, y, family, size, color, bold, italic, align, base, maxWidth) {
-		ctx.fillStyle = color || BLACK;
-		ctx.font = (italic ? "italic " : "") + (bold ? "bold " : "") + size + "px " + family;
-		ctx.textAlign = align || "start";
-		ctx.textBaseline = base || "alphabetic";
-		ctx.fillText(text, x, y, maxWidth);
+		this.ctx.fillStyle = color || BLACK;
+		this.ctx.font = (italic ? "italic " : "") + (bold ? "bold " : "") + size + "px " + family;
+		this.ctx.textAlign = align || "start";
+		this.ctx.textBaseline = base || "alphabetic";
+		this.ctx.fillText(text, x, y, maxWidth);
 	}
 	ice.graphics.Scene.prototype.textOutline = function(text, x, y, family, size, thickness, color, bold, italic, align, base, maxWidth) {
-		ctx.fillStyle = color || BLACK;
-		ctx.lineWidth = thickness || 1;
-		ctx.font = (italic ? "italic " : "") + (bold ? "bold " : "") + size + "px " + family;
-		ctx.textAlign = align || "start";
-		ctx.textBaseline = base || "alphabetic";
-		ctx.strokeText(text, x, y, maxWidth);
+		this.ctx.fillStyle = color || BLACK;
+		this.ctx.lineWidth = thickness || 1;
+		this.ctx.font = (italic ? "italic " : "") + (bold ? "bold " : "") + size + "px " + family;
+		this.ctx.textAlign = align || "start";
+		this.ctx.textBaseline = base || "alphabetic";
+		this.ctx.strokeText(text, x, y, maxWidth);
 	}
 	ice.graphics.Scene.prototype.image = function(img, x, y, width, height, clipX, clipY, clipWidth, clipHeight, taintCanvas) {
 		if(typeof img === "string") {
@@ -209,18 +213,18 @@ var ice = (function (ice) {
 				image.crossOrigin = "anonymous";
 			}
 			image.onload = function(e) {
-				ctx.drawImage(image, clipX, clipY, clipWidth, clipHeight, x, y, width, height);
+				this.ctx.drawImage(image, clipX, clipY, clipWidth, clipHeight, x, y, width, height);
 			}
 		}
 		else {
-			ctx.drawImage(img, clipX, clipY, clipWidth, clipHeight, x, y, width, height);
+			this.ctx.drawImage(img, clipX, clipY, clipWidth, clipHeight, x, y, width, height);
 		}
 	}
 	ice.graphics.Scene.prototype.save = function() {
-		return ctx.getImageData(0, 0, width, height);
+		return this.ctx.getImageData(0, 0, this.width, this.height);
 	}
 	ice.graphics.Scene.prototype.restore = function(imageData) {
-		ctx.putImageData(imageData, 0, 0);
+		this.ctx.putImageData(imageData, 0, 0);
 	}
 
 	// Duplicates
@@ -230,10 +234,6 @@ var ice = (function (ice) {
 	ice.graphics.Scene.prototype.rectangleOutline = ice.graphics.Scene.prototype.rectOutline;
 
 	// Methods
-
-	ice.graphics.method = function() {
-
-	}
 
 	return ice;
 }(ice || {}));
